30e9c6745bb5b8887df0dfa4bfad3a48f5a4485a
Ishay Tubi <ishay@neuron.vision>
2023-06-16 17:55:34 +0300

    Root predictor train loop.

---
+++ rootenizer/root_predictor_model.py
@@ -0,0 +1,94 @@
+'''
+The Neural network to predict clusters length -> root indices.
+'''
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from pathlib import Path as _P
+from rootenizer.rootenizer_dataset import WordClusterDataset
+
+default_weights_path = _P('rootenizer/word_cluster_model.pth')
+
+
+# Define the neural network architecture
+class WordClusterPredictor(nn.Module):
+    def __init__(self, weights_path:_P=default_weights_path, input_size=10, output_size=6, hidden_size=10):        
+        super(WordClusterPredictor, self).__init__()
+        self.output_size = output_size
+        self.weights_path = weights_path
+
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+        try:
+            weights  = torch.load(weights_path)
+            self.load_state_dict(weights)
+            print(f"Loaded weights from {weights_path}")
+        except BaseException as e:
+            print(f"Did not load weights from {weights_path} due to {e}")
+        self.eval()  # Default is eval
+
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+    def train_loop(self, word_cluster_dataset):
+
+        # Define loss function and optimizer
+        criterion = nn.MSELoss()
+        optimizer = optim.SGD(self.parameters(), lr=0.001)
+
+        # Create DataLoader for the dataset
+        dataloader = DataLoader(word_cluster_dataset, batch_size=1, shuffle=True)
+        self.train()
+
+        # Training loop
+        num_epochs = 10
+        for epoch in range(num_epochs):
+            for words, lengths in dataloader:
+                optimizer.zero_grad()
+                inputs = torch.Tensor(words)  # Convert words to tensor
+                targets = torch.Tensor(lengths)  # Convert lengths to tensor
+
+                # Forward pass
+                outputs = self(inputs)
+
+                # Compute loss
+                loss = criterion(outputs.squeeze(), targets)
+
+                # Backward pass and optimization
+                loss.backward()
+                optimizer.step()
+
+            print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}')
+
+        # Save the trained model
+        torch.save(self.state_dict(), default_weights_path)
+        print(f"Model was saved to {default_weights_path}")
+
+
+
+if __name__ == "__main__":
+
+    dataset = WordClusterDataset()
+    output_size = dataset.get_output_size()
+    word, labels = dataset[0]
+
+    self = WordClusterPredictor(output_size=output_size)
+    output = self(word)
+    print("labels", labels)    
+    print("output", output)
+    self.train_loop(dataset)
+
+    # Example usage of the trained model
+    the_word = 'הלכו'
+    input_vec = dataset.get_input_vectors_for_word(the_word)
+    predicted_length = self(input_vec)
+    predicted_length = predicted_length.detach().cpu().numpy()
+    print(f'{the_word} {predicted_length}')
---
+++ rootenizer/rootenizer_dataset.py
@@ -0,0 +1,66 @@
+'''
+Author: Ishay Tubi, Neuron Vision. 2023.
+Description:
+Rootenizer dataset translating word into vector reps and the root indices.
+'''
+
+import torch
+from torch.utils.data import Dataset
+from pathlib import Path as _P
+
+from pandas import DataFrame, read_csv
+
+dataset_path = _P('rootenizer/rootenizer - Dataset.csv')
+
+
+def load_dataset(dataset_path=dataset_path):
+    data = read_csv(dataset_path).set_index('Word')
+    return data
+
+
+# Custom dataset class.
+class WordClusterDataset(Dataset):
+    '''
+    We will try to use the ascii encoding instead of 1 hot encoding for a compact representation.
+    '''
+
+    def __init__(self, data:DataFrame=None, max_input_length=10):
+        self.data_table = data or load_dataset()
+        self.max_input_length = max_input_length
+
+    def get_output_size(self):
+        '''
+        '''
+        word, cluster_length = self[0] 
+        return len(cluster_length) 
+    
+    def get_input_vectors_for_word(self, word):
+        '''
+        Translate the string into an input vector. 
+        '''
+        t = torch.zeros(self.max_input_length, dtype=torch.float32)
+        for i, c in enumerate(word):
+            t[i] = ord(c)
+        return t
+
+    def __len__(self):
+        return len(self.data_table)
+
+    def __getitem__(self, index):
+        word = self.data_table.index[index]
+        row = self.data_table.loc[word]
+        values = row[['P', 'R1', 'I1', 'R2', 'I2', 'R3']].values.astype('float32')
+        values = torch.from_numpy(values)
+        t= self.get_input_vectors_for_word(word)
+        return t, values
+
+
+if __name__ == '__main__':
+    self = WordClusterDataset()
+    index = 0
+    key = self.data_table.index[index]
+    row = self.data_table.iloc[index]
+    word, clusters = key, row[['P', 'R1', 'I1', 'R2', 'I2', 'R3']].values.astype('float32')
+    tensor, clusters_length = self[0]
+    print(key, tensor, clusters_length)
+
